2ba26a06adb4d055d311e227f2a37380e3deadf5
Blake <blake@host-97.ziprecruiter.com>
2013-10-05 13:38:03 -0700

    Start a new Foundation project in Objective C called prog1.

---
+++ prog1/prog1/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  prog1
+//
+//  Created by Blake on 10/5/13.
+//  Copyright (c) 2013 ClassroomM-oc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
