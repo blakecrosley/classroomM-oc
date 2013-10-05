1765ff0b095d076c8b9c824a8fcad83713f3f239
Blake <blake@host-97.ziprecruiter.com>
2013-10-05 13:58:58 -0700

    Program 2.5

--- prog1/prog1/main.m
+++ prog1/prog1/main.m
@@ -12,9 +12,12 @@ int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
+        int sum, value1, value2;
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+        value1 = 50;
+        value2 = 25;
+        sum = value1 + value2;
+        NSLog(@"The sum of %i and %i is %i", value1, value2, sum);
         
     }
     return 0;
